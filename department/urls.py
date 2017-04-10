f200fb33fe1ce6d6f676e91553569d8bf5d01279
yswtrue <a365039311@gmail.com>
2017-04-11 00:05:03 +0800

    nothing

--- department/urls.py
+++ department/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
-import department.urls
-import project.urls
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^department/', include(department.urls)),
-    url(r'^project/', include(project.urls)),
+    # url(r'^admin/', admin.site.urls),
+    url('sign_up', views.sign_up)
 ]
--- department/views.py
+++ department/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def sign_up(request):
+    if request.method == 'GET':
+        return render(request, 'project/sign_up.html')
